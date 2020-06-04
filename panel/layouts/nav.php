4f0ae2f812176003812952346f1ccef28edbd37f
CuriousShade <thatsnotpublic@gmail.com>
2020-06-04 16:06:07 +0300

    uu

--- panel/layouts/nav.php
+++ panel/layouts/nav.php
@@ -170,7 +170,7 @@
           <img src="dist/img/user2-160x160.jpg" class="img-circle elevation-2" alt="User Image">
         </div>
         <div class="info">
-          <a href="#" class="d-block">Admin</a>
+          <a href="index.php" class="d-block">Admin</a>
         </div>
       </div>
 
