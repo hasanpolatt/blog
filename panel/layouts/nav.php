da58ba1a81f673fc5048c0d44cfea0fb2393f5a9
CuriousShade <thatsnotpublic@gmail.com>
2020-06-04 16:06:57 +0300

    uu

--- panel/layouts/nav.php
+++ panel/layouts/nav.php
@@ -164,7 +164,7 @@
 
     <!-- Sidebar -->
     <div class="sidebar">
-      <!-- Sidebar user panel (optional) -->
+
       <div class="user-panel mt-3 pb-3 mb-3 d-flex">
         <div class="image">
           <img src="dist/img/user2-160x160.jpg" class="img-circle elevation-2" alt="User Image">
