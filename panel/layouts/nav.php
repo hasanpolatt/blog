2b38fdd42837fa9bd343ebdef4db90d0134007f5
CuriousShade <thatsnotpublic@gmail.com>
2020-06-06 00:37:09 +0300

    update 2.1

--- panel/layouts/nav.php
+++ panel/layouts/nav.php
@@ -84,7 +84,7 @@ $ayarcek=$ayarsor->fetch(PDO::FETCH_ASSOC); ?>
                   <span class="float-right text-sm text-danger"><i class="fas fa-star"></i></span>
                 </h3>
                 <p class="text-sm">Call me whenever you can...</p>
-                <p class="text-sm text-muted"><i class="far fa-clock mr-1"></i> 4 Hours Ago</p>
+                <p class="text-sm text-muted"><i class="far fa-clock mr-1"></i> 5 Hours Ago</p>
               </div>
             </div>
             <!-- Message End -->
